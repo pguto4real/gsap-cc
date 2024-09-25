17a145a734225c4161d8fd93619257cc8fad3123
oluwamuibiclear <igbokweudoka@gmail.com>
2024-09-24 21:10:22 -0500

    gsap setup

--- app/page.js
+++ app/page.js
@@ -1,101 +1,97 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Link from "next/link";
+
+
+
+
+const animations = [
+  {
+    title: "GSAP To",
+    description:
+      "The to() method is used to animate a single element from a starting state to an ending state.",
+    path: "/gsapto",
+  },
+  {
+    title: "GSAP From",
+    description:
+      "The from() method is used to animate a single element from an ending state to a starting state.",
+    path: "/gsapfrom",
+  },
+  {
+    title: "GSAP FromTo",
+    description:
+      "The fromTo() method is used to animate a single element from a starting state to an ending state and vice versa.",
+    path: "/gsapfromto",
+  },
+  {
+    title: "GSAP Timeline",
+    description:
+      "The timeline() method is used to create a timeline to manage multiple animations.",
+    path: "/gsaptimeline",
+  },
+  {
+    title: "GSAP Stagger",
+    description:
+      "The stagger() method is used to animate multiple elements with a stagger effect.",
+    path: "/gsapstagger",
+  },
+  {
+    title: "GSAP ScrollTrigger",
+    description:
+      "The ScrollTrigger plugin is used to trigger animations based on the scroll position.",
+    path: "/gsapscrolltrigger",
+  },
+  {
+    title: "GSAP Text",
+    description: "Learn how to animate text with GSAP.",
+    path: "/gsaptext",
+  },
+];
+
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="container">
+      <div className="flex flex-col">
+        <h1 className="text-3xl font-bold text-zinc-50">GSAP Animations</h1>
+        <ol className="flex flex-col mt-10">
+          {animations.map((animation, index) => (
+            <li
+              key={index}
+              className="flex flex-row gap-2 p-5 hover:bg-zinc-800/50 rounded-lg"
+            >
+              <p>
+                <span className="text-sm font-bold text-zinc-50">
+                  {index + 1}.
+                </span>
+              </p>
+              <div className="flex flex-col gap-2 flex-1">
+                <Link
+                  href={animation.path}
+                  className="text-md font-semibold text-blue-600"
+                >
+                  {animation.title}
+                </Link>
+                <p className="text-gray-400 text-xs">{animation.description}</p>
+              </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+              <svg
+                className="size-6 text-gray-600 -rotate-90"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M10 13.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 1.414-1.414L10 13.586z"
+                />
+              </svg>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
